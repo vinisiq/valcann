3176256a88fca06ba2b2f9ce7bd02ef322bcf1eb
Vinicius Siqueira <viniciusdp2@outlook.com>
2022-07-02 16:17:10 -0300

    Changing logging methods

--- README.md
+++ README.md
@@ -1,25 +1,48 @@
+## Autor: Vinicius Siqueira da Silva
+## E-mail: viniciussiqueira.dev@outlook.com
+
+
 # Problema 1 | Automação de Ambientes Operacionais
 
-    Um dos principais desafios para um bom gerenciamento de infraestrutura, é implementar
-automação para permitir produtividade aos times de administração de tecnologia, bem como,
-minimizar ações humanas nos ambientes dos clientes.
+(OK)    Listar todos arquivos (nome, tamanho, data de criação, data da última modificação) localizados no
+        caminho /home/valcann/backupsFrom; 
 
-    O cliente “Acme Co.” possui um servidor centralizado de backup, o qual recebe arquivos de todos
-os demais servidores, move os dados para um volume temporário, para que deste volume os dados
-sejam copiados por uma ferramenta de backup externa.
+(OK)    Salvar o resultado no arquivo backupsFrom.log em /home/valcann/;
+        
+(OK)    Remover todos os arquivos com data de criação superior a 3 (três) dias; 
 
-    De forma a minimizar o nível de intervenção neste ambiente, você foi convocado a escrever um
-script (em Shell Script, Python ou qualquer outra tecnologia que preferir), para automatizar as
-seguintes ações:
+(OK)    Copiar todos os arquivos os arquivos com data de criação menor ou igual a 3 (três) dias em
+        home/valcann/backupsTo;
 
-    Listar todos arquivos (nome, tamanho, data de criação, data da última modificação) localizados no
-    caminho /home/valcann/backupsFrom;
+(OK)    Salvar o resultado no arquivo backupsTo.log em /home/valcann/.
 
-    Salvar o resultado no arquivo backupsFrom.log em /home/valcann/;
-    Remover todos os arquivos com data de criação superior a 3 (três) dias;
+## Considerações sobre a elaboração da resolução da problemática:
 
-    Copiar todos os arquivos os arquivos com data de criação menor ou igual a 3 (três) dias em
-    homevalcann/backupsTo;
+    O ínicio da elaboração da solução foi manual para uma melhor abstração do que deveria ser feito, era
+necessário entender da melhor forma possível o que era solicitado. Os rascunhos podem ser vistos na imagem a seguir.
+                #imagem
+    
+    Apesar de haver WSL2 Ubuntu 20.04 na minha máquina, optei pela utilização do Windows para execução
+da solução, uma vez que tenho no Ubuntu algumas configurações para projetos que estão em andamento. Logo, 
+a indicação de divisão da árvore de diretórios(pastas) terá simbologia "\\" no código app.py.
 
-    Salvar o resultado no arquivo backupsTo.log em /home/valcann/.
-    Fique a vontade para pesquisar ou reutilizar códigos disponíveis na comunidade.
+    Primeiramente, escolhi a linguagem Python, na versão 3.10.2, para solução da problemática, pois tenho mais
+praticidade com esta. Os módulos(bibliotecas) escolhidos para código foram:
+
+    - os, módulo nativo do python para manipulação de arquivos e pastas;
+    - datetime e time, módulos do python para manipulação/conversão de datas e horários;
+    - shutil, módulo utilizado para cópia e envio de arquivos na função copia_arquivos;
+    - loguru, módulo para criação de logs.
+
+### Explicando as funções:
+
+#### Função listar_arquivos():
+
+    A função listar_arquivos utiliza como parâmetro a indicação da pasta onde será listado os arquivos, neste caso, a pasta 'backupsFrom'. No ínicio da função podemos ver a chamada do objeto "logger.add('backupsFrom.log')", que cria ou atualiza o log (não duplica o log no caso da função ser chamada mais de uma vez). É necessário que o código consiga chegar a pasta backupsFrom para poder fazer a listagem dos arquivos que ali estão, neste caso, foi utilizada a chamada do objeto os.chdir(diretorio). 
+
+    Ademais, a primeira estrutura a ser utilizada foi o "for", que faz um loop dentro de "backupsFrom" por meio do método os.listdir() trazendo "arquivo" como resultado para os dados dos arquivos da referida pasta. Para imprimir no terminal o resultado da listagem, foi utilizado o método logger.info('mensagem') para retornar o resultado tanto no terminal quanto grava-lo no arquivo backupsFrom.log.
+
+#### Função remover_arquivos():
+
+    
+
--- app.py
+++ app.py
@@ -1,26 +1,17 @@
 import os
 import time
 import shutil
-import logging
 from datetime import timedelta
 from datetime import datetime
-from shutil import copyfile
-
+from loguru import logger
 
 
 def listar_arquivos(diretorio):
 
-    log_format = '%(asctime)s:%(levelname)s:%(filename)s:%(message)s'
+    logger.add('backupsFrom.log')
 
-    logging.basicConfig(filename='backupsFrom.log',
-                    # w -> sobrescreve o arquivo a cada log
-                    # a -> não sobrescreve o arquivo
-                    filemode='w',
-                    level=logging.DEBUG,
-                    format=log_format)
-
-    logger = logging.getLogger('root')
     os.chdir(diretorio)
+
 
     print('\n|----------------------------------------------------------|')
     print('|              INICIANDO LISTAGEM DE ARQUIVOS              |')
@@ -31,14 +22,11 @@ def listar_arquivos(diretorio):
             tamanho = os.path.getsize(arquivos)
             data_criacao = time.ctime(os.path.getctime(arquivos))
             data_alteracao = time.ctime(os.path.getmtime(arquivos))
-            logger.info(f'{nome}; Tamanho: {tamanho}KB; Data de Criacao: {data_criacao} ; Ultima alteracao: {data_alteracao} \n')
 
-            print("Nome: {}".format(nome))
-            print("Tamanho: {} KB".format(tamanho))
-            print("Data de criação: {}".format(data_criacao))
-            print("Data de última alteração: {}\n".format(data_alteracao))
+            logger.info(f'\n Nome: {nome};\n Tamanho: {tamanho}KB;\n Data de Criacao: {data_criacao};\n Ultima alteracao: {data_alteracao} \n')
 
-os.chdir('\\home\\valcann\\')
+    os.chdir('C:\home\\valcann')
+
 
 def remover_arquivos(diretorio):
     
@@ -58,63 +46,39 @@ def remover_arquivos(diretorio):
              os.remove(arquivos)
          else:
              print(f'Este arquivo > {arquivos} < ainda não ultrapassou 3 dias desde de criação.\n')
-    
-    os.chdir(diretorio)
 
-os.chdir('\\home\\valcann\\')
 
 def copiar_arquivos(diretorio_padrao):
-    
     diretorio_padrao = os.getcwd()
+    backupsTo = os.path.join(diretorio_padrao,'backupsTo')
+    logger.add("backupsTo.log")
     
-    log_format = '%(asctime)s:%(levelname)s:%(filename)s:%(message)s'
-
-    logging.basicConfig(filename='backupsTo.log',
-                    # w -> sobrescreve o arquivo a cada log
-                    # a -> não sobrescreve o arquivo
-                    filemode='w',
-                    level=logging.INFO,
-                    format=log_format)
-
-    logger = logging.getLogger('root')
-
-
+    if not os.path.isdir(backupsTo):
+        os.mkdir('backupsTo')
+    
     print('\n|----------------------------------------------------------|')
     print('|        INICIANDO CÓPIA DE ARQUIVOS PARA "backupsTo"      |')
     print('|----------------------------------------------------------|\n')
+    
     for arquivos in os.listdir(os.chdir('backupsFrom')):
         nome = os.path.basename(arquivos)
-        tamanho = os.path.getsize(arquivos)
         data_criacao = os.path.getctime(arquivos)
-        data_alteracao = time.ctime(os.path.getmtime(arquivos))
         data_criacao = datetime.fromtimestamp(data_criacao)
         ultima_data = data_criacao + timedelta(days=+3)
         
         if ultima_data >= datetime.today():
             shutil.copyfile(f'{diretorio_padrao}\\backupsFrom\\{arquivos}', f'{diretorio_padrao}\\backupsTo\\{arquivos}')
-            print(f'{arquivos} foi enviado para pasta backupsTo com sucesso\n')
-            os.chdir(diretorio_padrao)
-            logger.info(f'{nome}; Tamanho: {tamanho}KB; Data de Criacao: {data_criacao} ; Ultima alteracao: {data_alteracao} \n')         
-            os.chdir('backupsFrom')    
-
+            logger.info(f'\n Arquivo {nome} copiado para backupsTo com sucesso!\n')         
         else:
-             print(f'O arquivo {arquivos} tem mais de 3 dias e foi/será excluído.\n')
-             os.chdir(diretorio_padrao)
-             logger.info(f'{nome}; Tamanho: {tamanho}KB; Data de Criacao: {data_criacao} ; Ultima alteracao: {data_alteracao} \n')
-             os.chdir('backupsFrom') 
+            logger.error(f'\n O arquivo {nome} deveria ter sido excluído anteriormente, já que passa de 3 dias.\n')
 
-# Comandos para início:
+    os.chdir('C:\home\\valcann')
+
 
 listar_arquivos('backupsFrom')
-
 remover_arquivos('backupsFrom')
-
 copiar_arquivos('valcann')
 
 
-
-
-        
-        
-      
+     
 
